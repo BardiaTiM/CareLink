0c71b5a12725b937d13fafb9d67e0109877c7c63
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-08 22:22:28 -0800

    Add route for MainPage component

--- qds-front/src/App.js
+++ qds-front/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {Navbar} from './components/navbar';
-import {LandingPage} from './pages/index';
-import {Login} from './pages/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import { LandingPage } from './pages/index';
+import { Login } from './pages/login';
+import { MainPage } from './pages/mainPage'; // Import the MainPage component
 
 function App() {
     return (
         <Router>
-            <Navbar/>
+            <Navbar />
             <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/login" element={<Login />}/>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/main" element={<MainPage />} /> {/* Add this route for the MainPage */}
             </Routes>
         </Router>
     );
