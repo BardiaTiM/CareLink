a36e5607c92d7d9f62d7dd85c0f779fec3fc1df1
teihyung <teihyung@gmail.com>
2024-03-09 09:16:57 -0800

    added 404 page for users putting in wrong url and also changed string to update peer status for signup page and also allowed councilor to sign in

--- qds-front/src/App.js
+++ qds-front/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Authentication/AuthContext';
@@ -8,26 +7,28 @@ import { Login } from './pages/login';
 import { PrivateRoute } from './routing/PrivateRoute';
 import { SignUp } from './pages/signup';
 import { CouncilorLogin } from './pages/councilorLogin';
-import {Main} from "./pages/main";
+import { Main } from "./pages/main";
+import { NotFound } from './pages/404'; // Import the NotFound component
 
 function App() {
     return (
-      <AuthProvider>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path={"signup"} element={<SignUp />}/>
-                <Route path="/login" element={<Login />} />
+        <AuthProvider>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/signup" element={<SignUp />}/>
+                    <Route path="/login" element={<Login />} />
                     <Route path="/main" element={
                         <PrivateRoute>
                             <Main />
                         </PrivateRoute>
                     }/>
-                    <Route path="/councilorLogin" element={<CouncilorLogin/> } />
-            </Routes>
-        </Router>
-</AuthProvider>
+                    <Route path="/councilorLogin" element={<CouncilorLogin />} />
+                    <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 }
 
