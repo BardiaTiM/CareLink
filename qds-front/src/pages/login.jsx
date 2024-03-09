6f4c29b99a3ebb4d0c358da80fbad516d0c84e57
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-09 14:41:18 -0800

    Remove commented out code in login.jsx

--- qds-front/src/pages/login.jsx
+++ qds-front/src/pages/login.jsx
@@ -77,7 +77,6 @@ export function Login() {
 
     return (
         <div>
-            {/* <h1>Login</h1> */}
             <form onSubmit={handleSubmit}>
                 <input
                     name="email"
