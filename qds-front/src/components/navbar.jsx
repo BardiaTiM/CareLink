76e22a8f2cca346b67649473bf7a50c05c7f193f
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-09 01:10:32 -0800

    Added white logo to navbar

--- qds-front/src/components/navbar.jsx
+++ qds-front/src/components/navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../Authentication/AuthContext'; // Adjust the import path as necessary
 import "../style/Navbar.css";
+import logoImage from '../style/images/CareLink v1 - white.png'; // Update the path
 
 export function Navbar() {
     const { isAuthenticated, logout } = useAuth();
@@ -14,7 +15,10 @@ export function Navbar() {
 
     return (
         <nav className="navbar">
-            <Link to="/" className="brand logo-home">CareLink</Link>
+            <Link to="/" className="brand logo-home">
+                <span className="brand-text">CareLink</span>
+                <img src={logoImage} alt="CareLink Logo" className="logo-image" />
+            </Link>
             <ul className="navbar-nav">
                 {isAuthenticated && <li className="nav-item non-highlight"><Link to="/main">Main</Link></li>}
                 {!isAuthenticated && <li className="nav-item non-highlight"><Link to="/login">Login</Link></li>}
