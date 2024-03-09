9ef28c0c215e11b3327e239539a77beb7197f13f
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-09 10:38:40 -0800

    Added login form to splash

--- qds-front/src/pages/index.jsx
+++ qds-front/src/pages/index.jsx
@@ -1,13 +1,20 @@
-import "../style/Global.css";
-import "../style/Navbar.css";
+import React from 'react';
+import { Login } from './login'; // Import your Login component
+import '../style/Global.css';
+import '../style/Navbar.css';
+import '../style/Index.css'; // Create a new CSS file for styling
 
 export function LandingPage() {
-    return (
-        <>
-            <div>
-                <h1>QDS</h1>
-                <p>HomePage</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="splash-page">
+        {/* Splash page content */}
+        <div className="splash-content">
+          <h1>QDS</h1>
+          <p>HomePage</p>
+        </div>
+      </div>
+      <Login /> {/* Render the Login component */}
+    </>
+  );
 }
