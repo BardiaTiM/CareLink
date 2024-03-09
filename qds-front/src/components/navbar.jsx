61e90ada3d7b15ff0a75a3958be5c617bee20075
teihyung <teihyung@gmail.com>
2024-03-08 21:03:19 -0800

    Navbar nearly done with basic design with routing done properly

--- qds-front/src/components/navbar.jsx
+++ qds-front/src/components/navbar.jsx
@@ -3,10 +3,11 @@ import "../style/Navbar.css";
 
 export function Navbar() {
     return (
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/login">Login</Link></li>
-        </ul>
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item"><Link to="/">Home</Link></li>
+                <li className="nav-item"><Link to="/login">Login</Link></li>
+            </ul>
+        </nav>
     );
 }
