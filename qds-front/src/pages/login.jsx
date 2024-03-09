31e599c01e91e1846df9f8c7367494c094d01048
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-09 14:41:14 -0800

    Removed login heading

--- qds-front/src/pages/login.jsx
+++ qds-front/src/pages/login.jsx
@@ -77,7 +77,7 @@ export function Login() {
 
     return (
         <div>
-            <h1>Login</h1>
+            {/* <h1>Login</h1> */}
             <form onSubmit={handleSubmit}>
                 <input
                     name="email"
