6ba88b38f23a0820ef8523c1e2f2743047451508
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-09 18:47:13 -0800

    Renamed header and spaced oout components for my readability

--- qds-front/src/pages/RecomPage.jsx
+++ qds-front/src/pages/RecomPage.jsx
@@ -36,34 +36,43 @@ export function RecomPage() {
 
     return (
         <div className="RecomPage">
-            <h1>Main Recommendation</h1>
+            <h1>Peer For You</h1>
+
             {mainRecommendation && (
                 <div className="recommendation-card" onClick={toggleModal}>
                     <div className="person-info">
+
                         <img src={`https://via.placeholder.com/150?text=${mainRecommendation.username}`} alt="Person" />
+                        
                         <div className="info">
-                            <p>Name: {mainRecommendation.username}</p>
+                            <p>@{mainRecommendation.username}</p>
                         </div>
+
                         <div className="badges">
+
                         <img src={VerifiedHelper} alt="Badge 4" />
                         <img src={firstTimeHelper} alt="Badge 1" />
                         <img src={HelpedTenPeople} alt="Badge 2" />
                         <img src={HelpedTwentyFivePeople} alt="Badge 3" />
+
                         </div>
                     </div>
                 </div>
             )}
+
             {showModal && (
                 <div className="overlay" onClick={toggleModal}>
                     <RecomModal onClose={toggleModal} description={mainRecommendation.description} />
                 </div>
             )}
+
             {!showMore && recommendations.length > 0 && (
                 <div className="view-more-container">
-                    <button onClick={toggleMore}>View More Recommendations</button>
+                    <button onClick={toggleMore}>View More</button>
                 </div>
             )}
-            {showMore && <MoreRecommendations recommendations={recommendations} onClose={toggleModal} />} {/* Pass recommendations and toggleModal function to MoreRecommendations component */}
+
+            {showMore && <MoreRecommendations recommendations={recommendations} onClose={toggleModal} />}
         </div>
     );
 }
